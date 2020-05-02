0fae73ef826da8eecb3cf2e9c742727cf762bcec
Jack Reilly <jackdreilly@gmail.com>
2020-05-02 13:57:16 +0200

    isDisposed

--- lib/deferrable.dart
+++ lib/deferrable.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/widgets.dart';
 
 mixin Deferrable<T extends StatefulWidget> on State<T> {
+  bool _isDisposed = false;
   final _deferreds = <Function>[];
   void defer(Function f) => _deferreds.add(f);
+  bool get isDisposed => _isDisposed;
 
   @override
   void dispose() {
+    _isDisposed = true;
     _deferreds.forEach((f) => f());
     super.dispose();
   }
